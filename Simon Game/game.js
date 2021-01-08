c452152ab5b80aa5ec86e6f2836ec6044b524f0a
Shubham Raj <65071251+Shub-hamburger@users.noreply.github.com>
2021-01-08 22:48:15 +0530

    Add files via upload

---
+++ Simon Game/game.js
@@ -0,0 +1,73 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+var gamePattern = [];
+var started = false;
+var level = 0;
+
+$(".btn").on("click", function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * (3 - 0));
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+  var chosenButton = $("#" + randomChosenColour);
+  chosenButton.fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {;
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
+
+function startOver(){
+  gamePattern = [];
+  started = false;
+  level = 0;
+
+}
